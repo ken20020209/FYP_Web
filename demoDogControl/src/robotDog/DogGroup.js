5772043411d752bb1264973df640bdc73694c8d4
ken20020209 <ken20020209@gmail.com>
2024-02-06 14:21:10 +0800

    add controller class and dogGroupFrontend class

---
+++ demoDogControl/src/robotDog/DogGroup.js
@@ -0,0 +1,88 @@
+import ROSLIB from "roslib";
+
+
+// this class group the dog controller
+// it will same same command to dog group
+// it not  work for navigation
+
+export class DogGroupFrontend {
+    actionList = {
+        Lie_Down: ()=>this.doAction(1),
+        Stand_Up: ()=>this.doAction(2),
+        Crawl: ()=>this.doAction(3),
+        Turn_Around: ()=>this.doAction(4),
+        Mark_Time: ()=>this.doAction(5),
+        Squat: ()=>this.doAction(6),
+        Turn_Roll: ()=>this.doAction(7),
+        Turn_Pitch: ()=>this.doAction(8),
+        Turn_Yaw: ()=>this.doAction(9),
+        Axis_3: ()=>this.doAction(10),
+        pee: ()=>this.doAction(11),
+        Sit_Down: ()=>this.doAction(12),
+        Wave_Hand: ()=>this.doAction(13),
+        Stretch: ()=>this.doAction(14),
+        Wave_Body: ()=>this.doAction(15),
+        Swing: ()=>this.doAction(16),
+        Pray: ()=>this.doAction(17),
+        Seek: ()=>this.doAction(18),
+        Handshake: ()=>this.doAction(19),
+        Reset: ()=>this.doAction(255),
+      };
+    dogControllerList = {};
+    constructor(ros) {
+        this.ros = ros;
+    }
+    
+    //todo: the addDog and remove dog may use another parameter (not use dogController)
+    addDog(dogController) {
+        if(dogController.name in this.dogControllerList){
+            throw new Error("Dog name already exist");
+        }
+        this.dogList[dogController.name] = dogController;
+    }
+    removeDog(dogController) {
+        if(dogController.name in this.dogControllerList){
+            delete this.dogControllerList[dogController.name];
+        }
+    }
+    //action control
+    doAction(action){
+        for (const key in this.dogControllerList) {
+            this.dogControllerList[key].movement.doAction(action);
+        }
+    }
+    //movement control
+    move_forward(speed){
+        for (const key in this.dogControllerList) {
+            this.dogControllerList[key].movement.move_forward(speed);
+        }
+    }
+    move_backward(speed){
+        for (const key in this.dogControllerList) {
+            this.dogControllerList[key].movement.move_backward(speed);
+        }
+    }
+    move_left(speed){
+        for (const key in this.dogControllerList) {
+            this.dogControllerList[key].movement.move_left(speed);
+        }
+    }
+    move_right(speed){
+        for (const key in this.dogControllerList) {
+            this.dogControllerList[key].movement.move_right(speed);
+        }
+    }
+    turn_left(speed){
+        for (const key in this.dogControllerList) {
+            this.dogControllerList[key].movement.turn_left(speed);
+        }
+    }
+    turn_right(speed){
+        for (const key in this.dogControllerList) {
+            this.dogControllerList[key].movement.turn_right(speed);
+        }
+    }
+
+    //camera custom by control camera class
+
+}
