4be20efdae4a4405d7005a092b99970bf46f689a
ken20020209 <ken20020209@gmail.com>
2024-02-08 20:18:44 +0800

    remove 2d add ros2d demo (it is not work)

--- demoDogControl/src/components/controller.vue
+++ demoDogControl/src/components/controller.vue
@@ -4,13 +4,15 @@
       <movement :controller=controller></movement>
       <camera :controller=controller></camera>
       <action :controller=controller></action>
-  
+      <map2d :controller=controller></map2d>
+      
     </main>
 </template>
 <script setup>
 import movement from './movement.vue';
 import camera from './camera.vue';
 import action from './action.vue';
+import map2d from './map2d.vue';
 import { Controller } from '../robotDog/Controller';
 
 const ros= defineProps(['ros']);
---
+++ demoDogControl/src/components/map2d.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>Map</h1>
+    <div id="map2d"></div>
+</template>
+<script setup>
+
+import { ref, onMounted } from 'vue';
+import {ROS2D} from "/node_modules/ros2d/build/ros2d";
+
+const controller= defineProps(['controller']);
+const ros= controller.controller.ros;
+
+const viewer=ROS2D.Viewer({
+  divID: 'map',
+  width: 640,
+  height: 480
+});
+
+// const controller= defineProps(['controller']);
+// const movement = controller.controller.movement;
+
+</script>
