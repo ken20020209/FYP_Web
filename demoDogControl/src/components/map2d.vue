6b8ff7f08251649838bc33549921eaba4bdd8225
ken20020209 <ken20020209@gmail.com>
2024-02-10 19:56:35 +0800

    remove ros2d package that cant run with ros2. it is design for ros 1
    add page receive map topic message

--- demoDogControl/src/components/map2d.vue
+++ demoDogControl/src/components/map2d.vue
@@ -1,31 +1,28 @@
 <template>
     <h1>Map</h1>
     <div id="map2d"></div>
+    <p>{{ msg}}</p>
 </template>
 <script setup>
 
 import { ref, onMounted } from 'vue';
-import * as ROS2D from "@ken20020209/ros2d";
+import ROSLIB from "roslib";
 
 const controller= defineProps(['controller']);
 const ros= controller.controller.ros;
-
+const msg = ref('');
 
 const init=()=>{
-  const viewer=new ROS2D.Viewer({
-    divID: 'map2d',
-    width: 640,
-    height: 480
+  const map2d =new ROSLIB.Topic({
+    ros: ros,
+    name: '/map',
+    messageType : 'nav_msgs/msg/OccupancyGrid',
   });
-  // Setup the map client.
-  var gridClient = new ROS2D.OccupancyGridClient({
-      ros : ros,
-      rootObject : viewer.scene
-    });
-    // Scale the canvas to fit to the map
-  gridClient.on('change', function() {
-    viewer.scaleToDimensions(gridClient.currentGrid.width, gridClient.currentGrid.height);
-    viewer.shift(gridClient.currentGrid.pose.position.x, gridClient.currentGrid.pose.position.y);
+
+  map2d.subscribe(function(message) {
+    console.log('Received message on ');
+    console.log(message);
+    msg.value = message;
   });
   
 }
