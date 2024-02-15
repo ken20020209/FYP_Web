9a933e05789742f29c570f89fa054123554aa82e
ken20020209 <ken20020209@gmail.com>
2024-02-16 02:04:16 +0800

    add ros2djs package support map display demo

--- demoDogControl/src/components/map2d.vue
+++ demoDogControl/src/components/map2d.vue
@@ -1,32 +1,66 @@
 <template>
-    <h1>Map</h1>
-    <div id="map2d"></div>
-    <!-- <p>{{ msg}}</p> -->
+  <h1>Map</h1>
+  <div id="map2d"></div>
+  <!-- <p>{{ msg}}</p> -->
+  <p>
+    center x:<input type="number" min="0" max="600" step="1" v-model="centerX" />
+    center y:<input type="number" min="0" max="500" step="1" v-model="centerY" />
+  </p>
+  <p>
+    scale:<input type="number" min="0" max="10" step="0.1" v-model="zoomScale" />
+    <button @click="zoom">scale center</button>
+  </p>
 </template>
 <script setup>
 
 import { ref, onMounted } from 'vue';
 import ROSLIB from "roslib";
+import * as ROS2D from "ros2d";
 
-const controller= defineProps(['controller']);
-const ros= controller.controller.ros;
+const controller = defineProps(['controller']);
+const ros = controller.controller.ros;
 const msg = ref('');
+var zoomView;
+const init = () => {
+  // const map2d = new ROSLIB.Topic({
+  //   ros: ros,
+  //   name: '/map',
+  //   messageType: 'nav_msgs/msg/OccupancyGrid',
+  // });
 
-const init=()=>{
-  const map2d =new ROSLIB.Topic({
+  // map2d.subscribe(function (message) {
+  //   console.log('Received message on ');
+  //   console.log(message);
+  //   msg.value = message;
+  // });
+  var viewer = new ROS2D.Viewer({
+    divID: 'map2d',
+    width: 600,
+    height: 500
+  });
+  var gridClient = new ROS2D.OccupancyGridClient({
     ros: ros,
-    name: '/map',
-    messageType : 'nav_msgs/msg/OccupancyGrid',
+    rootObject: viewer.scene,
   });
-
-  map2d.subscribe(function(message) {
-    console.log('Received message on ');
-    console.log(message);
-    msg.value = message;
+  // Scale the canvas to fit to the map
+  gridClient.on('change', function () {
+    viewer.scaleToDimensions(gridClient.currentGrid.width, gridClient.currentGrid.height);
+    viewer.shift(gridClient.currentGrid.pose.position.x, gridClient.currentGrid.pose.position.y);
   });
-  
+  zoomView = new ROS2D.ZoomView({
+    rootObject: viewer.scene
+  });
+  // zoomView.startZoom(300,250);
+  // zoomView.zoom(2);
 }
-
+const zoomScale=ref(1);
+const centerX=ref(300);
+const centerY=ref(250);
+const zoom=()=>{
+  // console.log(zoomScale.value);
+  zoomView.startZoom(300,250);
+  zoomView.zoom(zoomScale.value);
+}
 
 onMounted(() => {
   init();
