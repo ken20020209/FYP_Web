6dd56ce058e881e7878a796d8b86c4a7ca59d46d
ken20020209 <ken20020209@gmail.com>
2024-03-05 23:02:59 +0800

    Add navigation and slam control functionality

--- demoDogControl/src/robotDog/Controller.js
+++ demoDogControl/src/robotDog/Controller.js
@@ -13,19 +13,108 @@ import { BasicNavigator } from "./NavigationSever";
 export class Controller {
   constructor(ros,name='',domainID=16) {
     this.name=name;
+    this.namespace = "";
     this.ros = ros;
     this.ip="";
     this.domainID=domainID;
+
+    // components
     this.movement = new Movement(ros);
     this.camera = new Camera(ros);
     this.action = new Action(ros);
     this.navigation = new BasicNavigator(ros);
-
-    // console.log(this.ros.socket.url);
+    
     //extract ip from url
     let url = this.ros.socket.url;
     let ip = url.split(":")[1].split("//")[1];
     this.ip = ip;
     // console.log(this.ip);
+
+    this.navigation_on=false;
+    this.slam_on=false;
+
+    // topics
+    this.navigation_on_sub = new ROSLIB.Topic({
+      ros: this.ros,
+      name: this.namespace+"/controller/navigation",
+      messageType: "std_msgs/msg/Bool",
+    });
+    this.slam_on_sub = new ROSLIB.Topic({
+      ros: this.ros,
+      name: this.namespace+"/controller/slam",
+      messageType: "std_msgs/msg/Bool",
+    });
+    this.navigation_on_sub.subscribe(this._navigationOnCallback);
+    this.slam_on_sub.subscribe(this._slamOnCallback);
+    
+    // services
+    this.navigation_switch_service = new ROSLIB.Service({
+      ros: this.ros,
+      name: this.namespace+"/controller/navigation_switch",
+      serviceType: "message/srv/SwitchService",
+    });
+    this.slam_switch_service = new ROSLIB.Service({
+      ros: this.ros,
+      name: this.namespace+"/controller/slam_switch",
+      serviceType: "message/srv/SwitchService",
+    });
   }
+
+  _navigationOnCallback(msg){
+    this.navigation_on=msg.data;
+  }
+  _slamOnCallback(msg){
+    this.slam_on=msg.data;
+  }
+
+  /**
+   * @function setNavigation
+   * @description Set the navigation status
+   * @param {boolean} status - Status of the navigation
+   */
+  setNavigation(status) {
+
+    if(typeof(status)!=Boolean){
+      throw new Error("Invalid status type it should boolean");
+    }
+    let request = new ROSLIB.ServiceRequest({
+      switch_service: status,
+    });
+    this.navigation_switch_service.callService(request, _setNavigationCallback);
+  }
+  _setNavigationCallback(msg){
+    console.log("Navigation status set to: "+msg.result);
+  }
+  startNavigation(){
+    this.setNavigation(true);
+  }
+  stopNavigation(){
+    this.setNavigation(false);
+  }
+
+  /**
+   * @function setSlam
+   * @description Set the slam status
+   * @param {boolean} status - Status of the slam
+   */
+  setSlam(status) {
+    if(typeof(status)!=Boolean){
+      throw new Error("Invalid status type it should boolean");
+    }
+    let request = new ROSLIB.ServiceRequest({
+      switch_service: status,
+    });
+    this.slam_switch_service.callService(request, _setSlamCallback);
+  }
+  _setSlamCallback(msg){
+    console.log("Slam status set to: "+msg.result);
+  }
+  startSlam(){
+    this.setSlam(true);
+  }
+  stopSlam(){
+    this.setSlam(false);
+  }
+
+
 }
